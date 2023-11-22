b8679c860ead0af89c0b42f30725fa666dc6a333
Patricia Tran <PTRAN@MITRE.ORG>
2023-11-22 15:27:56 -0500

    commands to pull csv master files

--- tools/makeIJEMappingFromCSVtoHTML.rb
+++ tools/makeIJEMappingFromCSVtoHTML.rb
@@ -1,5 +1,6 @@
 # NOTE: Before running this script, you should download a local copy of the latest spreadsheets from https://github.com/nightingaleproject/vital_records_sandbox_ig
-
+# Invoke-Webrequest https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv?raw=true -Outfile "./input/mapping/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv"
+# Invoke-Webrequest https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Profile_Intros.csv?raw=true -Outfile "./input/mapping/BFDR_Profile_Intros.csv"
 # ruby tools/makeIJEMappingFromCSVtoHTML.rb input/mapping/BFDR_Profile_Intros.csv input/mapping/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv 
 
 #require 'open-uri'
