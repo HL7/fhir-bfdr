f2388f0532cb5713704e7fe18601ad53bccae1d0
Patricia Tran <PTRAN@MITRE.ORG>
2023-11-27 11:04:21 -0500

    update intro note

--- tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb
+++ tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb
@@ -7,12 +7,12 @@
 
 #method 2:
 #require 'open-uri'
-#download1 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.xlsx?raw=true')
-#IO.copy_stream(download1, 'input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.xlsx')
-#download2 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Profile_Intros.xlsx?raw=true')
-#IO.copy_stream(download2, 'input/mapping/BFDR_Profile_Intros.xlsx')
-#download3 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Forms_Mapping.xlsx?raw=true')
-#IO.copy_stream(download3, 'input/mapping/BFDR_Forms_Mapping.xlsx')
+#download1 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv?raw=true')
+#IO.copy_stream(download1, 'input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv')
+#download2 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Profile_Intros.csv?raw=true')
+#IO.copy_stream(download2, 'input/mapping/BFDR_Profile_Intros.csv')
+#download3 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Forms_Mapping.csv?raw=true')
+#IO.copy_stream(download3, 'input/mapping/BFDR_Forms_Mapping.csv')
 
 #run:
 #ruby tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb input/mapping/BFDR_Profile_Intros.csv input/mapping/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv input/mapping/BFDR_Forms_Mapping.csv
