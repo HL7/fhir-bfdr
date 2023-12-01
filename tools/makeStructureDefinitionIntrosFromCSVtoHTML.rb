dc1d68ed92638789f70c50e48052544c32766f92
rpassas <rpassas@mitre.org>
2023-11-30 21:49:14 -0500

    script updates

--- tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb
+++ tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb
@@ -6,13 +6,11 @@
 #Invoke-Webrequest https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Forms_Mapping.csv?raw=true -Outfile "./input/mapping/BFDR_Forms_Mapping.csv"
 
 #method 2:
-# require 'open-uri'
-# download1 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv?raw=true')
-# IO.copy_stream(download1, 'input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv')
-# download2 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Profile_Intros.csv?raw=true')
-# IO.copy_stream(download2, 'input/mapping/BFDR_Profile_Intros.csv')
-# download3 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Forms_Mapping.csv?raw=true')
-# IO.copy_stream(download3, 'input/mapping/BFDR_Forms_Mapping.csv')
+require 'open-uri'
+download1 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv?raw=true')
+IO.copy_stream(download1, 'input/images/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv')
+download2 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Forms_Mapping.csv?raw=true')
+IO.copy_stream(download2, 'input/mapping/BFDR_Forms_Mapping.csv')
 
 #run:
 #ruby tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb input/mapping/BFDR_Profile_Intros.csv input/mapping/IJE_File_Layouts_Version_2021_FHIR-2023-02-22-All-Combined.csv input/mapping/BFDR_Forms_Mapping.csv
@@ -596,11 +594,7 @@ def createSDIntros(pIG, pProfileIntrosSpreadsheet, pIJEMappingSpreadsheet, pForm
         end
         formName = row[FORMS_FORM_COL].to_s.partition('Standard').last
         if row[FORMS_FIELD_COL].to_s.strip.empty?
-          if row[FORMS_CONTEXT_COL].to_s.empty?
-            fhirField = "-"
-          else
-            fhirField = row[FORMS_CONTEXT_COL].to_s.partition('.').last 
-          end     
+          fhirField = "-"
         else
           fhirField = row[FORMS_FIELD_COL].to_s
         end
